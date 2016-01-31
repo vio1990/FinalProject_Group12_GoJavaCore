d845175487f643ebe0475c5aa054ee8ce0ba3743
Ivan Ozerian <vio1990@ukr.net>
2016-01-31 21:42:38 +0200

    commit

--- src/com/ozerian/finalProject/Main.java
+++ src/com/ozerian/finalProject/Main.java
@@ -1,25 +1,35 @@
 package com.ozerian.finalProject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         try {
-            int continues;
+            int continues = 0;
             Choice.greeting();
             do {
                 String choice = Choice.enteredChoice();
 
                 switch (choice) {
                     case "1":
-                        int number = DataInput.enteredInputDataInt();
-                        String binaryNumber = Converter.binaryNumber(number);
-                        DataOutput.printResult(binaryNumber);
+                        int intNumber = DataInput.enteredInputDataInt();
+                        String binNumber = Converter.toBinary(intNumber);
+                        DataOutput.printResult(binNumber);
                         DataOutput.printThanks();
+                        continues = DataOutput.asking();
+                        break;
+                    case "2":
+                        String binaryNumber = DataInput.enteredInputDataBinary();
+                        String integerNumber = Converter.toInteger(binaryNumber);
+                        DataOutput.printResult(integerNumber);
+                        DataOutput.printThanks();
+                        continues = DataOutput.asking();
+                        break;
+                    case "3":
+                        DataOutput.printThanks();
+                        continues = 2;
+                        break;
                 }
-                continues = DataOutput.asking();
             } while (continues != 2);
 
         } catch (IOException ex) {
