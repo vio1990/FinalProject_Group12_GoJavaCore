6bdd3ebbc3a7751a832080e3159fe2a1c67ee39c
Ivan Ozerian <vio1990@ukr.net>
2016-07-01 17:18:21 +0300

    refactoring.

--- src/com/goit/gojava/group12/project/program/ConverterUtils.java
+++ src/com/goit/gojava/group12/project/program/ConverterUtils.java
@@ -19,14 +19,33 @@ public class ConverterUtils {
 
     private static void handleNegative(int number, StringBuilder result) {
 
+        inversionOfDigits(number, result);
+        result.reverse();                            // reversing the bits
+        additionOfBits(result);
+
+        String finalReturnCode = result.toString();
+
+        result.setLength(0);                         // rebooting our stringBuilder
+
+        addNegativeBits(result, finalReturnCode);
+        result.append(finalReturnCode);              // add finalReturnCode bits
+    }
+
+    private static void addNegativeBits(StringBuilder result, String finalReturnCode) {
+        for (int i = 0; i < Integer.SIZE - finalReturnCode.length(); i++) {
+            result.append(1);                        // add negative indicating bits with digit - "1"
+        }
+    }
+
+    private static void inversionOfDigits(int number, StringBuilder result) {
         while (number < 0) {
             int modulus = (number % 2) == 0 ? 1 : 0;          // inverting all digits of the same positive Integer value
             result.append(modulus);
             number /= 2;
         }
+    }
 
-        result.reverse();                            // reversing the bits
-
+    private static void additionOfBits(StringBuilder result) {
         for (int i = result.length() - 1; i >= 0; i--) {     // Add "1" to the reversed bits
             if (result.charAt(i) == '1') {
                 result.setCharAt(i, '0');
@@ -35,14 +54,6 @@ public class ConverterUtils {
                 break;
             }
         }
-
-        String finalReturnCode = result.toString();
-        result.setLength(0);                         // rebooting our stringBuilder
-
-        for (int i = 0; i < Integer.SIZE - finalReturnCode.length(); i++) {
-            result.append(1);                        // add negative indicating bits with digit - "1"
-        }
-        result.append(finalReturnCode);              // add finalReturnCode bits
     }
 
     private static void handlePositive(int number, StringBuilder result) {
@@ -56,7 +67,6 @@ public class ConverterUtils {
 
     public static String toInteger(String binNumber) {
         StringBuilder buf = new StringBuilder(binNumber).reverse();
-
         int result = 0;
         for (int i = binNumber.length() - 1; i >= 0; i--) {
 
